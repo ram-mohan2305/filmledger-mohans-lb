0eea3ad9ad7b72832fbec9bbcda189914ce03f1b
Ram Mohan Adabala <rammohanadabala235@gmail.com>
2025-04-09 19:46:40 +0530

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react'
+import { Button, Navbar, TextInput, NavbarCollapse, NavbarLink, NavbarToggle, Dropdown, Avatar, DropdownHeader, DropdownItem, DropdownDivider } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
+import {useSelector} from 'react-redux'
 import React from 'react'
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -27,9 +29,36 @@ export default function Header() {
             <Button className='w-14 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon />
             </Button>
-            <Link to="/sign-in">
-                <Button className='bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'>Sign In</Button>
-            </Link>
+            {currentUser ? (
+                <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                        <Avatar
+                            alt='user'
+                            img={currentUser.profilePicture}
+                            rounded
+                        />
+                    }
+                >
+                    <DropdownHeader>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
+                    </DropdownHeader>
+                    <Link to={'/dashboard?tab=profile'}>
+                        <DropdownItem>Profile</DropdownItem>
+                    </Link>
+                    <DropdownDivider />
+                    <DropdownItem>Sign Out</DropdownItem>
+                </Dropdown>
+            ): 
+            (
+                <Link to="/sign-in">
+                    <Button className='bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'>Sign In</Button>
+                </Link>
+            )
+        }
+            
             <NavbarToggle />
         </div>
         <NavbarCollapse>
